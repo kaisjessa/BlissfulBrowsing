fac37bb53793e78036844be7d1706241f9d85172
Kais Jessa <kaisjessa@gmail.com>
2020-05-02 15:00:46 -0400

    Basic right-click functionality

--- background.js
+++ background.js
@@ -1,5 +1,14 @@
-chrome.runtime.onInstalled.addListener(function() {
-	chrome.storage.sync.set({color: '#3aa757'}, function() {
-		console.log("The color is green.");
-	});
+factCheck = function(userSelection) {
+	let userText = userSelection.selectionText;
+	console.log(userText);
+};
+
+chrome.contextMenus.create({
+ title: "Fact Check",
+ contexts:["selection"],
+ onclick: factCheck
 });
+
+
+
+console.log("test");
