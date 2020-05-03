e741f5fd36bbf1fcc389665ca68f01eb3f490944
Kais Jessa <kaisjessa@gmail.com>
2020-05-02 23:49:05 -0400

    Current

--- background.js
+++ background.js
@@ -1,12 +1,6 @@
-function getText() {
-	let allInfo = chrome.windows.getCurrent;
-	let text = allInfo;
-	return text;
-}
-
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
   if (changeInfo.status == 'complete') {
-  	console.log(getText());
+
   }
 })
 
--- toxicityprediction.js
+++ toxicityprediction.js
@@ -22,6 +22,7 @@ let allText = getText().split("\n");
 //console.log(allText);
 let splitText = allText.filter(word => word.length > 2);
 //console.log(splitText);
+console.log("NUMBER OF PHRASES:", splitText.length);
 
 // The minimum prediction confidence.
 const threshold = 0.5;
@@ -29,32 +30,33 @@ let filter = [];
 let num = 0;
 // Load the model. Users optionally pass in a threshold and an array of
 // labels to include.
+// WORKS BUT SLOW
 // toxicity.load(threshold).then(model => {
 //   const sentences = splitText;
-//   for(let i=0; i<splitText.length; i++) {
+//   for(let i=0; i<sentences.length; i++) {
 //     model.classify([sentences[i]]).then(prediction => {
 //       num = prediction[6].results[0].probabilities[1]
-//       console.log(sentences[i], num);
 //       if(num >= threshold) {
 //         document.body.innerHTML = document.body.innerHTML.replace(sentences[i], '[removed due to toxicity]');
+//         console.log(sentences[i], "REPLACED");
 //       }
 //     })
 //   }
-//   console.log(filter);
 // });
 
+// FACT BUT CRASHES A LOT
 toxicity.load(threshold).then(model => {
-  const sentences = splitText;
+  let sentences = splitText;
   model.classify(sentences).then(prediction => {
-    for(let j=0; j<prediction[6].results.length;j++) {
+    for(let j=0; j<Math.min(prediction[6].results.length, 5);j++) {
       filter.push(prediction[6].results[j].match);
       //console.log(prediction[6].results[j].match);
     };
     for(let i=0; i<filter.length; i++) {
       if(filter[i]==true) {
         document.body.innerHTML = document.body.innerHTML.replace(sentences[i], '[removed due to toxicity]');
+        console.log(sentences[i], "REPLACED");
       }
-      console.log("TEXT REPLACED");
     };
   });
 
