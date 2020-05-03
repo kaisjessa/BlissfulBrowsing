bdc19ace7be105722dc2c96cbefa23501be51d63
Kais Jessa <kaisjessa@gmail.com>
2020-05-02 22:42:31 -0400

    Works well but still slow

--- toxicityprediction.js
+++ toxicityprediction.js
@@ -2,30 +2,62 @@ function getText(){
     return document.body.innerText
 }
 
-let allText = getText();
-let splitText = allText.split("\n").filter(word => word.length > 2);
-console.log(splitText);
-var elements = document.getElementsByTagName('*');
-console.log(elements);
+function getContent() {
+  words = [];
+  var elements = document.getElementsByTagName('*');
+  for (var i = 0; i < elements.length; i++) {
+      var element = elements[i];
+      for (var j = 0; j < element.childNodes.length; j++) {
+          var node = element.childNodes[j];
+          if (node.nodeType === 3) {
+              var text = node.nodeValue;
+              words.push(text);
+          }
+      }
+  }
+  return words;
+}
+
+let allText = getText().split("\n");
+//console.log(allText);
+let splitText = allText.filter(word => word.length > 2);
+//console.log(splitText);
 
 // The minimum prediction confidence.
-const threshold = 0.9;
+const threshold = 0.5;
 let filter = [];
 let num = 0;
 // Load the model. Users optionally pass in a threshold and an array of
 // labels to include.
+// toxicity.load(threshold).then(model => {
+//   const sentences = splitText;
+//   for(let i=0; i<splitText.length; i++) {
+//     model.classify([sentences[i]]).then(prediction => {
+//       num = prediction[6].results[0].probabilities[1]
+//       console.log(sentences[i], num);
+//       if(num >= threshold) {
+//         document.body.innerHTML = document.body.innerHTML.replace(sentences[i], '[removed due to toxicity]');
+//       }
+//     })
+//   }
+//   console.log(filter);
+// });
+
 toxicity.load(threshold).then(model => {
   const sentences = splitText;
-  for(let i=0; i<splitText.length; i++) {
-    model.classify([sentences[i]]).then(prediction => {
-      num = prediction[6].results[0].probabilities[1]
-      console.log(sentences[i], num);
-      if(num >= threshold) {
+  model.classify(sentences).then(prediction => {
+    for(let j=0; j<prediction[6].results.length;j++) {
+      filter.push(prediction[6].results[j].match);
+      //console.log(prediction[6].results[j].match);
+    };
+    for(let i=0; i<filter.length; i++) {
+      if(filter[i]==true) {
         document.body.innerHTML = document.body.innerHTML.replace(sentences[i], '[removed due to toxicity]');
       }
-    })
-  }
-  console.log(filter);
+      console.log("TEXT REPLACED");
+    };
+  });
+
 });
 
 console.log("PREDICTONS LOADED");
